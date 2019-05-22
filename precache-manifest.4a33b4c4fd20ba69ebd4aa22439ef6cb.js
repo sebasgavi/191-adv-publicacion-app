1e39fa6e368928dd1a38ad13bb7a296166cefc17
Sebastián Gaviria <vagyriag@hotmail.com>
2019-05-22 14:25:59 -0500

    hola

---
+++ precache-manifest.4a33b4c4fd20ba69ebd4aa22439ef6cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9edffec62f172f384f8b",
+    "url": "/static/js/main.50b90b16.chunk.js"
+  },
+  {
+    "revision": "2828756b045a3452ca79",
+    "url": "/static/js/2.893eaff8.chunk.js"
+  },
+  {
+    "revision": "9edffec62f172f384f8b",
+    "url": "/static/css/main.20c49a11.chunk.css"
+  },
+  {
+    "revision": "42aeeeeb9d5f2cff384e79f1fc571e0b",
+    "url": "/index.html"
+  }
+];
